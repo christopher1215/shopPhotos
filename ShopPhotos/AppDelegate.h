c06c9772900622c9ed04bbad43e3e5e04a0a2277
stanleyCoder <addcn@addcndeimac.local>
2016-12-18 08:51:54 +0800

    Initial Commit

---
+++ ShopPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShopPhotos
+//
+//  Created by addcn on 16/12/18.
+//  Copyright © 2016年 addcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShopPhotos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShopPhotos
+//
+//  Created by addcn on 16/12/18.
+//  Copyright © 2016年 addcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
